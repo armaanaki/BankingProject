d4fe4a50099a2e49eab37abc7e4e9470da8faef3
Armaan Esfahani <armaanaki@gmail.com>
2015-09-17 17:23:54 -0400

    Moved Hello World command to be called from a function.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,10 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		printHelloWorld();
+	}
+	
+	public static void printHelloWorld(){
 		System.out.println("Hello World!");
 	}
-
 }
